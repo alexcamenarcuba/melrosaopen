868f88222cc7871bd216779765ba69f25b49da49
Alex Vinicius Camenar Cuba <alex.camenar@gmail.com>
2023-05-31 10:33:24 -0300

    first commit

---
+++ painel/config.php
@@ -0,0 +1,41 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Admin');
+
+// HTTP
+//define('HTTP_SERVER', 'https://melrosakids.com.br/painel/');
+//define('HTTP_CATALOG', 'https://melrosakids.com.br/');
+define('HTTP_SERVER', 'http://melrosaopen/painel/');
+define('HTTP_CATALOG', 'http://melrosaopen/');
+
+// DIR
+//define('DIR_OPENCART', '/home/u306864128/domains/melrosakids.com.br/public_html/');
+define('DIR_OPENCART', 'C:/laragon/www/melrosaopen/');
+define('DIR_APPLICATION', DIR_OPENCART . 'painel/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storageam0sa29be7ox/');
+//define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+//define('DB_HOSTNAME', '127.0.0.1');
+define('DB_HOSTNAME', '212.1.208.101');
+define('DB_USERNAME', 'u306864128_CbVb1');
+define('DB_PASSWORD', 'mPHAGqVgp7');
+define('DB_DATABASE', 'u306864128_D8DGg');
+define('DB_PREFIX', 'ylvb_');
+define('DB_PORT', '3306');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
